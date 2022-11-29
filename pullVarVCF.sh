916b4424f407431009c2002574a0741828f654db
Aidan Cruickshank <hapachew@gmail.com>
2022-11-28 20:30:34 -0500

    Major updates to opt_SV - first iteration which should be runnable.

--- pullVarVCF.sh
+++ pullVarVCF.sh
@@ -5,11 +5,12 @@
 
 #Options passable are: 
 #   - s (for SV) *or* -m (for MUGQIC)  
-#Both options take four arguments bound by commas (i.e. XXX,XXX,TEST,PATH). Arguments passed must be: 
-#   start coordinate (int)
-#   end coordinmate (int)
+#Both options take four arguments bound by commas (i.e. 17,43044295,43125364,BRCA1,your/path/here). Arguments passed must be: 
+#   chromosome (int)
+#   start coordinate (int) - as per hg38
+#   end coordinmate (int) - as per hg38
 #   user supplied identifier for the output directory (for example the gene name for the region of interest) (string)
-#   path to the directory containing all data (i.e. a "root" directory under which all data files exist, files or further directories)
+#   path to the directory containing all data (i.e. a "root" directory under which all data files exist (for -s), or a path to a file containing the data (for -m))
 #Add -c *first* for use in Compute Canda clusters (this will load the necessary modules)
 
 while getopts 'cs:m:h' opt; do
@@ -25,7 +26,7 @@ while getopts 'cs:m:h' opt; do
       set -f
       IFS=,
       array=($OPTARG)
-      ./opt_SV ${array[0]} ${array[1]} ${array[2]} ${array[3]} 
+      ./opt_SV ${array[0]} ${array[1]} ${array[2]} ${array[3]} ${array[4]}
       ;;
 
     m)
@@ -34,21 +35,21 @@ while getopts 'cs:m:h' opt; do
       set -f
       IFS=,
       array=($OPTARG)
-      ./opt_MUGQIC ${array[0]} ${array[1]} ${array[2]} ${array[3]}
+      ./opt_MUGQIC ${array[0]} ${array[1]} ${array[2]} ${array[3]} ${array[4]}
       ;;
 
     h)
-      echo "Usage: $(basename $0) [-c] ([-s INT,INT,STRING,STRING] or [-m INT,INT,STRING,STRING])"
+      echo "Usage: $(basename $0) [-c] ([-s INT,INT,INT,STRING,STRING] or [-m INT,INT,INT,STRING,STRING])"
       exit 0
       ;;
 
     :)
-      echo -e "option requires an argument.\nUsage: $(basename $0) [-c] ([-s INT,INT,STRING,STRING] or [-m INT,INT,STRING,STRING])"
+      echo -e "option requires an argument.\nUsage: $(basename $0) [-c] ([-s INT,INT,INT,STRING,STRING] or [-m INT,INT,INT,STRING,STRING])"
       exit 1
       ;;
 
     ?)
-      echo -e "Invalid command option.\nUsage: $(basename $0) [-c] ([-s INT,INT,STRING,STRING] or [-m INT,INT,STRING,STRING])"
+      echo -e "Invalid command option.\nUsage: $(basename $0) [-c] ([-s INT,INT,INT,STRING,STRING] or [-m INT,INT,INT,STRING,STRING])"
       exit 1
       ;;
   esac
