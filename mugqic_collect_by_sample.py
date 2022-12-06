b60f282542471711b66dab48642d4c2ae69b8853
Aidan Cruickshank <hapachew@gmail.com>
2022-12-06 13:38:30 -0500

    added clinvar annotations

--- mugqic_collect_by_sample.py
+++ mugqic_collect_by_sample.py
@@ -1,12 +1,12 @@
 import sys
 import csv
-import os.path
+import os
 
 #Argument 1 is the path to the opt_MUGQIC output file
 #Argument 2 is the output directory
 #This script will output variants present by patient/sample
 
-print("Beginning grouping by patient")
+print("Finished BCFTools filtering. Beginning grouping by patient")
 
 samples = dict()
 
@@ -20,10 +20,14 @@ with open(sys.argv[1]) as variants_tsv:
                 continue
             else: 
 
+                #Annotate data with ClinVar variant information
                 #Add files to samples dict if not already in dict, and add data
                 #Dont add file to samples dict if it's already in there, but add any relevant data
 
                 variantDetails = line[-6:]
+                command = "esearch -db clinvar -query \"(" + str(variantDetails[1]) + " [Base Position]) AND " + str(variantDetails[0][-2:]) + " [Chromosome]\" | efetch -format docsum | xtract -pattern DocumentSummary -sep \",\" -element title clinical_significance/description"
+                clinVarData = os.popen(command).read()
+                variantDetails.append(clinVarData)
 
                 for element in line: 
                     if element not in samples.keys() and element.startswith("PA"): 
