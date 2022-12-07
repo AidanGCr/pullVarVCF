2e5c8a2688c0b721cb306c6d1f3a49fe9af2bcc3
Aidan Cruickshank <hapachew@gmail.com>
2022-12-06 20:04:31 -0500

    altered python script data formatting to fix column alignments

--- mugqic_collect_by_sample.py
+++ mugqic_collect_by_sample.py
@@ -1,12 +1,13 @@
 import sys
 import csv
 import os
+import re 
 
 #Argument 1 is the path to the opt_MUGQIC output file
 #Argument 2 is the output directory
 #This script will output variants present by patient/sample
 
-print("Finished BCFTools filtering. Beginning grouping by patient")
+print("Finished BCFTools filtering. Beginning grouping by patient.")
 
 samples = dict()
 
@@ -26,24 +27,25 @@ with open(sys.argv[1]) as variants_tsv:
 
                 variantDetails = line[-6:]
                 command = "esearch -db clinvar -query \"(" + str(variantDetails[1]) + " [Base Position]) AND " + str(variantDetails[0][-2:]) + " [Chromosome]\" | efetch -format docsum | xtract -pattern DocumentSummary -sep \",\" -element title clinical_significance/description"
-                clinVarData = os.popen(command).read()
+                clinVarData = re.split('\t|\n', os.popen(command).read())
                 variantDetails.append(clinVarData)
 
                 for element in line: 
                     if element not in samples.keys() and element.startswith("PA"): 
-                        fileName = os.path.join(sys.argv[2], element + ".csv")
+                        fileName = os.path.join(sys.argv[2], element + ".tsv")
                         samples[element] = fileName
                         newFile = open(fileName, "x")
-                        removeFirst = str(variantDetails)[1:]
-                        removeLast = removeFirst[:-1]
-                        newFile.write(removeLast + "\n")
+                        for characteristic in variantDetails: 
+                            newFile.write(str(characteristic) + "\t")
+                        newFile.write("\n")
                         newFile.close()
                     elif element in samples.keys() and element.startswith("PA"): 
                         fileName = samples.get(element)
                         newFile = open(fileName, "a")
-                        removeFirst = str(variantDetails)[1:]
-                        removeLast = removeFirst[:-1]
-                        newFile.write(removeLast + "\n")
+                        for characteristic in variantDetails: 
+                            newFile.write(str(characteristic) + "\t")
+                        newFile.write("\n")
                         newFile.close()
+                        
 
                     
