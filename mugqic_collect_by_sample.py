8b2ab07e6b2ce5654fd0174722655337b6f8b919
Aidan Cruickshank <hapachew@gmail.com>
2022-12-11 14:52:35 -0500

    updated script to format to put possibly pathogenic variants in another column

--- mugqic_collect_by_sample.py
+++ mugqic_collect_by_sample.py
@@ -9,6 +9,9 @@ import re
 
 print("Finished BCFTools filtering. Beginning grouping by patient.")
 
+#Alter this list to include any files that do not follow the typical PAXXXXX naminng format
+nonStandardSampleNames = ["2-2419861", "2-2419864", "N2824", "N3873", "S1223", "S1274", "S2417", "S2557", "S585", "S664"] 
+
 samples = dict()
 
 with open(sys.argv[1]) as variants_tsv: 
@@ -28,10 +31,13 @@ with open(sys.argv[1]) as variants_tsv:
                 variantDetails = line[-6:]
                 command = "esearch -db clinvar -query \"(" + str(variantDetails[1]) + " [Base Position]) AND " + str(variantDetails[0][-2:]) + " [Chromosome]\" | efetch -format docsum | xtract -pattern DocumentSummary -sep \",\" -element title clinical_significance/description"
                 clinVarData = re.split('\t|\n', os.popen(command).read())
-                variantDetails.append(clinVarData)
+                if "Pathogenic" in clinVarData: 
+                    variantDetails.append("\t" + str(clinVarData))
+                else: 
+                    variantDetails.append(clinVarData)
 
                 for element in line: 
-                    if element not in samples.keys() and element.startswith("PA"): 
+                    if element not in samples.keys() and (element.startswith("PA") or element in nonStandardSampleNames): 
                         fileName = os.path.join(sys.argv[2], element + ".tsv")
                         samples[element] = fileName
                         newFile = open(fileName, "x")
@@ -39,7 +45,7 @@ with open(sys.argv[1]) as variants_tsv:
                             newFile.write(str(characteristic) + "\t")
                         newFile.write("\n")
                         newFile.close()
-                    elif element in samples.keys() and element.startswith("PA"): 
+                    elif element in samples.keys() and (element.startswith("PA") or element in nonStandardSampleNames): 
                         fileName = samples.get(element)
                         newFile = open(fileName, "a")
                         for characteristic in variantDetails: 
